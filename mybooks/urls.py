a0e00a2c88bef20896f7f113ee31b29660cf8d24
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-03-17 10:46:04 +0100

    Revert "vymazal jsem url"
    This reverts commit 8f798bc1b68e49fad15a3c46047d2eae71398a77.

--- mybooks/urls.py
+++ mybooks/urls.py
@@ -3,4 +3,11 @@ from django.urls import path
 
 from books import views
 
-
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", views.IndexView.as_view(), name="index"),
+    path("books", views.BooksView.as_view(), name="book_list"),
+    path("books/<slug:slug>", views.OneBook.as_view(), name="one_book"),
+    path("books/create/", views.BookCreateView.as_view(), name="book_create"),
+    path("form", views.ContactFormView.as_view(), name="form_view"),
+]
