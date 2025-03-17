ec6ca1019ccf0872f933cacec98462603a86c444
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-03-17 10:34:52 +0100

    test na revert commit

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -23,7 +24,7 @@ class Book(models.Model):
         validators=[MinValueValidator(10), MaxValueValidator(1000)],
     )
     is_bestseller = models.BooleanField(
-        default=False, verbose_name="Je to bestseller?: "
+        default=False, verbose_name="Je to bestseller?:"
     )
     created = models.DateTimeField(
         auto_now_add=True,
@@ -33,6 +34,7 @@ class Book(models.Model):
         auto_now=True,
         verbose_name="update zaznamu",
     )
+    slug = models.SlugField(blank=True, unique=True)
 
     class Meta:
         """Meta funkce na ordering"""
@@ -44,3 +46,8 @@ class Book(models.Model):
 
     def __repr__(self) -> str:
         return f"Book(ID={self.pk},title='{self.title}', pages={self.pages}, Bestseller={self.is_bestseller})"
+
+    def save(self, *args, **kwargs):
+        if not self.slug or self.slug == "":
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
