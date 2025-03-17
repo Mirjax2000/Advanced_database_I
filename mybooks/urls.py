a94c5cc5801c14b9cf2c1d5fb74eb33d08e03450
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-03-17 10:49:14 +0100

    Revert "smazala jsem to"
    This reverts commit b5275d285e892443aeb6b467f13a78e7fc4fe7e5.

--- mybooks/urls.py
+++ mybooks/urls.py
@@ -20,9 +20,11 @@ from django.urls import path
 
 from books import views
 
-handler404 = "books.views.custom_404"
-
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("books", views.BooksView.as_view(), name="book_list"),
+    path("books/<slug:slug>", views.OneBook.as_view(), name="one_book"),
+    path("books/create/", views.BookCreateView.as_view(), name="book_create"),
+    path("form", views.ContactFormView.as_view(), name="form_view"),
 ]
